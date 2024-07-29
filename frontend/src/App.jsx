db2554f8e790169b63cd74ed28b2fa2450cfbc2e
kunal Dugar <yoyokvunal@gmail.com>
2024-07-29 19:44:33 +0530

    initial files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
+import { logo } from './assets/logo.svg'
+
+function App() {
+
+
+  return (
+    <div className='text-red-400'>
+      <h1>hello </h1>
+    </div>
+  )
+}
+
+export default App
